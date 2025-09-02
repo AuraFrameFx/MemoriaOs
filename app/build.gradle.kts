9eaa3ea5e387a6ed316040edcd2f9396797be076
AuraKai <wehttam1989@gmail.com>
2025-09-01 20:20:01 -0600

    Add Firebase Analytics dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -229,6 +229,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.firebase.analytics)
 
     // Compose UI
     implementation(libs.bundles.compose)
